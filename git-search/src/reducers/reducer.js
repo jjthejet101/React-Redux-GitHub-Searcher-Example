08d732d35994c76767e815be278e5f1119ee09d5
Unknown <justinkjetton@gmail.com>
2018-02-05 07:00:54 -0600

    React-redux.1

---
+++ git-search/src/reducers/reducer.js
@@ -0,0 +1,14 @@
+import Redux from 'redux';
+
+let initialState = {
+
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
